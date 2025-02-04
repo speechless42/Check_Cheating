f6270f624b429c7f08ddec71b4df652507ffbac3
speechless42 <94534629+speechless42@users.noreply.github.com>
2025-02-04 18:14:18 +0800

    again vercel serverless

--- api/proxy.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-    const response = await fetch('https://od.moi.gov.tw' + req.url, {
-      method: req.method,
-    });
-  
-    const data = await response.json();
-    res.status(response.status).json(data);
-  }
-  
