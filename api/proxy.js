00ecff3d3d6cdc8bddc35cb4d2c96f69fbff77e7
speechless42 <94534629+speechless42@users.noreply.github.com>
2025-02-04 21:00:24 +0800

    vvercel

--- api/proxy.js
+++ api/proxy.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
     const { path } = req.query;
-    console.log(req.query)
     const response = await fetch(`https://od.moi.gov.tw/api/v1/rest/datastore/${path}`, {
-        method: req.method,
+        method: 'GET',
     });
 
     const data = await response.json();
