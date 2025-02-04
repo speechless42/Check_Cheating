215d43e5ae397dc5e6eb0ba25fc808aceebb73ea
speechless42 <94534629+speechless42@users.noreply.github.com>
2025-02-04 20:56:36 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,10 +1,8 @@
 export default async function handler(req, res) {
     const { path } = req.query;
+    console.log(req.query)
     const response = await fetch(`https://od.moi.gov.tw/api/v1/rest/datastore/${path}`, {
         method: req.method,
-        headers: {
-            'Content-Type': 'application/json'
-        }
     });
 
     const data = await response.json();
